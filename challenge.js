84eaacb66aab16f15dde53d6fd7f31136269aec1
Melissa Kouadio <angemelisk@gmail.com>
2022-12-29 16:54:39 +0000

    week2 day3 daily-challenge

---
+++ challenge.js
@@ -0,0 +1,27 @@
+/**
+ * Write a JavaScript program that recreates the pattern below.
+|*  
+|* *  
+|* * *  
+|* * * *  
+|* * * * *
+|* * * * * *
+|*/
+for(let i = 0; i <1; i++) {
+    console.log("*");
+    for (let i = 0; i <= 3; i++) {
+      console.log("**");
+    }
+    for (let i = 0; i <= 4; i++) {
+        console.log("***");
+      }
+    for (let i = 0; i <= 5; i++) {
+        console.log("****");
+      }
+    for (let i = 0; i <= 6; i++) {
+        console.log("*****");
+    }
+    for (let i = 0; i <= 7; i++) {
+        console.log("******");
+    }
+  }
